d1754da32288985ff4eb8cf9e9e79797a6b9ee49
Christian <christianperez.rbb@gmail.com>
2023-12-13 13:04:18 +0100

    feat: create common linkButton.

---
+++ src/common/LinkButton/LinkButton.jsx
@@ -0,0 +1,15 @@
+
+import "./LinkButton.scss";
+
+import { useNavigate } from "react-router-dom";
+
+export const LinkButton = ({ path, tittle }) => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="linkButtonDesign" onClick={()=>navigate(path)}>
+            {tittle}
+        </div>
+    )
+}
