1a26f09d5d9f9bf7eb7d3f09ee5e62e33194f73f
Christian <christianperez.rbb@gmail.com>
2023-12-13 13:44:45 +0100

    feat: create Login page.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,59 @@
 
+import { useState } from "react";
 import "./Login.css";
+import { CustomInput } from "../../common/CustomInput/CustomInput";
 
+
+
+export const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [credenciales, setCredenciales] = useState({
+        username: "",
+        password: "",
+    });
+
+    const functionHandler = (e) => {
+        setCredenciales((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const logMe = () => {
+
+        logUser(credenciales)
+        .then(
+            resultado => {
+                console.log(resultado)
+
+                setTimeout(() => {
+                    navigate("/");
+                },500);
+            }
+        )
+        .catch(error => console.log(error));
+
+    }
+
+    return (
+        <div className="loginDesign">
+            <CustomInput
+                design={"inputDesign"}
+                type={"text"}
+                name={"username"}
+                placeholder={""}
+                functionProp={functionHandler}
+            />
+            <CustomInput
+                design={"inputDesign"}
+                type={"password"}
+                name={"password"}
+                placeholder={""}
+                functionProp={functionHandler}
+            />
+            <div className="buttonSubmit" onClick={logMe}>Log Me!</div>
+        </div>
+    );
+};
