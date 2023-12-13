fbb287c2d8220125bddbc35ee7fa08f67931e828
Christian <christianperez.rbb@gmail.com>
2023-12-13 20:50:56 +0100

    update: updated custom input for profile.

--- src/common/CustomInput/CustomInput.jsx
+++ src/common/CustomInput/CustomInput.jsx
@@ -2,15 +2,18 @@
 import React from "react";
 import "./CustomInput.css";
 
-export const CustomInput = ({ design, type, name, placeholder, functionProp }) => {
+export const CustomInput = ({ design, type, name, placeholder, value, disabled, functionBlur, functionProp }) => {
 
     return (
         <input
+            disabled={disabled}
             className={design}
             type={type}
             name={name}
             placeholder={placeholder}
+            value={value||undefined}
             onChange={(e)=>functionProp(e)}
+            onBlur={(e)=>functionBlur(e)}
         />
     )
 }
